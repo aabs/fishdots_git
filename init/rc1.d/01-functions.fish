2731941d3c22b3eb742dd0867532ac7b59e23dad
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-07 21:15:06 +1000

    passing on the args from sync

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -3,10 +3,11 @@ define_subcommand fdg save on_fdg_save "save all edits"
 define_subcommand fdg sync on_fdg_sync "save all edits then push to origin"
 
 function fdg_save -e on_fdg_save
-    git add -A .; git commit -m  "$argv"; 
+    git add -A .
+    git commit -m  "$argv"
 end
 
 function fdg_sync -e on_fdg_sync
-    fdg_save
+    fdg_save $argv
     git push origin master
 end

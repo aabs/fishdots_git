f558137dc348f948b05d4fd27895ff777f214278
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-07 21:03:15 +1000

    wip

---
+++ init/rc1.d/01-functions.fish
@@ -0,0 +1,12 @@
+define_command fdg "fishdots plugin for doing useful things with git"
+define_subcommand fdg save on_fdg_save "save all edits"
+define_subcommand fdg sync on_fdg_sync "save all edits then push to origin"
+
+function fdg_save -e on_fdg_save
+    git add -A .; git commit -m  "$argv"; 
+end
+
+function fdg_sync -e on_fdg_sync
+    fdg_save
+    git push origin master
+end

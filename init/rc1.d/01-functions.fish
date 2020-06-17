7131cb04cdac82d01573696d1fe5e2cc9254ba86
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-06-17 11:56:21 +1000

    new tags command

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -2,6 +2,7 @@ define_command fdg "fishdots plugin for doing useful things with git"
 define_subcommand fdg save on_fdg_save "save all edits"
 define_subcommand fdg sync on_fdg_sync "save all edits then push to origin"
 define_subcommand_nonevented fdg release fdg_release "(<ver> <msg>) Update release version, tag and push"
+define_subcommand_nonevented fdg tag fdg_tag "(<ver> <msg>) release, without use of latest-release.txt file"
 define_subcommand_nonevented fdg ed fdg_find_and_edit "find a file in the current repo and edit"
 
 function fdg_save -e on_fdg_save
@@ -16,6 +17,12 @@ function fdg_sync -e on_fdg_sync
       set branchname "master"
     end
     git push origin $branchname
+end
+
+function fdg_tag -a ver msg -d "release, without use of latest-release.txt file"
+  fdg sync "$msg"
+  git tag -a $ver -m "$msg"
+  git push origin --tags
 end
 
 function fdg_release -a ver msg -d "a function to prepare a fishdots style release"
